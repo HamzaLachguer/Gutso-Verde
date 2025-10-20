2ad28b66202bcaab89d40e570046b03d0209771d
hamzalachguer <lachguerhmza@gmail.com>
2025-10-20 03:34:47 +0200

    About section

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,11 @@ import { initHero } from "./modules/hero/hero.js";
 
 initHeader();
 initHero();
+
+
+const text = "with global flavors, offering a diverse, seasonal menu. The ambiance balances modern elegance with rustic charm, perfect for a relaxed brunch, sophisticated lunch, or intimate evening."
+
+
+text.split(" ").forEach((word, index) => {
+  console.log(`<span>${word}</span>`)
+})
